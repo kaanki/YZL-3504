5fa457c0c934e4b7f55495006af99bbdc13f1e1e
fatmaomac <fatmaomac6@gmail.com>
2023-02-18 19:17:49 +0300

    commit

---
+++ YZL-3504/FatmaOmac/javascript/05-02-23-js-veriable-types-exercises/js/main.js
@@ -0,0 +1,34 @@
+let p1 = 3;
+let p2 = 4;
+let sum = p1+p2;
+let output="bu toplama işleminin sonucu" + sum;
+
+console.log (output);
+
+/* ÖRNEK 
+ boolean tipinde bir değişken oluşturunuz ve console.log aracılığı ile konsola depişkenin tipi boolean'dır yazdırınız.*/
+
+
+/*
+--birinci yöntem--
+
+let hasCar= false ;
+console.log("bu değişkenin tipi:", typeof hasCar, "'dır")
+*/
+
+//---ikinci yöntem--
+let hasCar= false ;
+let myType=typeof hasCar ;
+let outputSentence = 'Değişkenin tipi' + '' + myType + '\'dır.'
+
+/* statement, expression, literals  */
+// hata alamadan değere çözülebiliyorsa expressiondur.(totalNum-3-4-3+4....)
+
+const PI= 3.14 ;
+let totalNum = 3 + 4 + PI ;
+let test ;
+
+console.log (test);
+
+let users= ["burak", "fatma", "kaan", "derya",]
+users[0] = "mustafa"
---
+++ YZL-3504/FatmaOmac/javascript/18-02-23-conditional-statements/js/main.js
@@ -0,0 +1,94 @@
+/* js ia a loosely typed language */
+// conditianol-koşul
+ 
+let season = "winter";
+
+if( season == "winter") {
+    console.log ("jumper")
+}
+else if (season=="summer") {
+    console.log ("t-shirt")
+}
+else if (season=="fall") {
+    console.log ("sweatshirt")
+}
+else if (season=="spring") {
+    console.log ("hat")
+}
+else {
+    console.log ("Invalid season detected!")
+}
+
+/* == (coersion )vs ===( strict equality check) */
+
+console.log (1 === "1") // number değeri string değere eşitliyoruz iki tane eşittir true verir ama üç tane eşittir daha garanti olur ve onu eşitlemez o yüzden 3 tane eşittir kullanırız.
+
+
+let team = "fb"
+
+switch (team)  {
+    case "fb":
+        console.log ("kanarya");
+        break;
+    case "gs" :
+        console.log ("aslan");
+        break;
+    case "bjk":
+        console.log ("kartal");
+        break;
+    default:
+        console.log ("Team not found!")    
+}
+
+/* --- and (&&), or (||) */
+
+let firstname  = "fatma"
+let password ="1234"
+
+if (firstname === "fatma" && password==="1234" ) {
+    console.log ("you are in!")
+}
+else {
+    console.log ("nop!")
+}
+
+let weatherCondition = "rainy"
+
+if (weatherCondition === "rainy" || weatherCondition === "snowy") {
+    console.log ("Take your umberalla!")
+}
+
+// not (!), notEqual (!==) -> eşit değilse demek
+
+let brand = "samsung";
+
+if (brand !== "iphone") {
+    console.log ("Go to android store")
+}
+
+let isYoung = false ;
+
+if (isYoung) {
+    console.log("not retired")
+}
+
+if (!isYoung){
+    console.log("retired")
+}
+
+let hasPet= true ;
+
+if (hasPet){
+    console.log("hayvansever")
+}
+if(!hasPet) {
+    console.log("değil")
+}
+
+/* falsy values : 0, false, -0, null, undefined, NaN */
+
+let myCarCount = 0;
+
+if (myCarCount) {
+    console.log ("you are rich")
+}
